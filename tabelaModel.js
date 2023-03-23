3138f8a17723970c9687d2e562d91ffcbb6b4fbd
Graziely Tavares <grazitds2005@gmail.com>
2023-03-22 21:48:33 -0300

    Update tabelaModel.js

--- tabelaModel.js
+++ tabelaModel.js
@@ -2,39 +2,37 @@ const Sequelize = require('sequelize');
 
 const connection = require('../database/database.js');
 
-/*
-PARAMETROS DO MÉTODO DEFINE
-1 - NOME DA TABELA - STRING
-2 - OBJETO JSON: 
-                NOME DO CAMPO DA TABELA
-                TIPO DE DADO DO CAMPO DA TABELA
-                REGRAS DO CAMPO DA TABELA (NULL, NOT NULL, DEFAULT...ETC)
-*/
+
 const tabelaModel = connection.define(
-    'Cliente',
+    'cliente',
     {
         cpf:{
         type: Sequelize.INTEGER(11),
         primaryKey: true,
         allowNull: false
         },
-        nome:{
+        nomeDono:{
         type: Sequelize.STRING(100),
         allowNull: false
         },
-        endereço:{
+        endereco:{
         type: Sequelize.STRING(100),
         allowNull: false
         },
         telefone:{
         type: Sequelize.STRING(10),
         allowNull: false
+        },
+        nomeAnimal:{
+        type: Sequelize.STRING(100),
+        allowNull: false
         }
+        
     }
         
     
 );
 
-tabelaModel.sync({force:true});
+//tabelaModel.sync({force:true});
 
 module.exports = tabelaModel;
