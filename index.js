6f984c932c90bfd5656a222a57e4bb2d3ccaf895
Graziely Tavares <grazitds2005@gmail.com>
2023-03-22 21:48:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Importando o pacote express
 const  express = require('express');
-const routesCliente = require('./route/routesCliente');
+const routesCliente = require('./routes/routesCliente')
 
 /*executando o Express*/
 const app = express();
@@ -10,13 +10,6 @@ app.use(express.json());
 console.log('testando!')
 
 app.use('/', routesCliente);
-
-/*Webserver capaz de receber requisições
-
-1. Porta Lógica -OBRIGATÓRIO-
-2. CallBack     -  OPCIONAL -
-
-*/
 
 // local host
 app.listen(3000,()=>{
